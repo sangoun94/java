dde3e8719bc1d1d84fb36e1accd9b320fcd28bb3
HSW <sangoun94@naver.com>
2021-03-31 16:05:47 +0900

    first commit

---
+++ src/main/java/kakarott/example/javaexercise/ReferenseMethod.java
@@ -0,0 +1,30 @@
+package kakarott.example.javaexercise;
+
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+public class ReferenseMethod {
+    /**
+     * 메소드 참조
+     * 2021.03.31
+     * @param args
+     */
+    public static void main(String[] args) {
+        /*List<Integer> list = Arrays.asList(30, 40, 50, 60, 70);
+        list = new ArrayList<>(list);
+
+        Consumer<List<Integer>> consumer = i -> Collections.reverse(i);
+        consumer.accept(list);
+        System.out.println(list);*/
+
+        // 메소드 참조
+        List<String> ls = new ArrayList<>();
+        ls.add("30");
+        ls.add("40");
+        ls.add("50");
+
+        Consumer<List<String>> consumer = Collections::reverse;
+        consumer.accept(ls);
+    }
+}
